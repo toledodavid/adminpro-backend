e530b5a1c2aae7713b12ebdc017486144d06f7d2
David Toledo <dtoledoz0207@gmail.com>
2020-10-27 17:10:37 -0600

    create-custom-middleware-validate-fields

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,4 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 const User = require('../models/user.model');
 
 
@@ -16,14 +15,6 @@ const getUsers = async(request, res) => {
 const createUser = async (request, res = response) => {
 
   const { name, email, password } = request.body;
-
-  const errors = validationResult(request);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
 
   const existEmail = await User.findOne({email});
 
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,6 +5,8 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { getUsers, createUser } = require('../controllers/users.controller');
 
+const { validateFields } = require('../middlewares/validate-fields');
+
 const router = Router();
 
 router.get('/', getUsers);
@@ -13,7 +15,8 @@ router.post('/',
   [
     check('name', 'name is required').not().isEmpty(),
     check('email', 'email is required').isEmail(),
-    check('password', 'password is required').not().isEmpty()
+    check('password', 'password is required').not().isEmpty(),
+    validateFields
   ],
   createUser
 );
