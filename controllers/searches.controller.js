290f86488d3cd07c5b4b46f6648a0f36d356c6f6
David Toledo <dtoledoz0207@gmail.com>
2020-11-05 17:07:30 -0600

    search-by-name-in-users-doctors-hospitals-collections

--- controllers/searches.controller.js
+++ controllers/searches.controller.js
@@ -1,12 +1,30 @@
 const { response } = require('express');
+const User = require('../models/user.model');
+const Doctor = require('../models/doctors.model');
+const Hospital = require('../models/hospital.model');
 
 
-const getSearchAll = (request, res = response) => {
+const getSearchAll = async(request, res = response) => {
+
   const search = request.params.search;
+
+  const regularExpresion = RegExp(search, 'i');
+
+  // const users = await User.find({name: regularExpresion});
+  // const doctors = await Doctor.find({name: regularExpresion});
+  // const hospitals = await Hospital.find({name: regularExpresion});
+
+  const [users, doctors, hospitals] = await Promise.all([
+    User.find({name: regularExpresion}),
+    Doctor.find({name: regularExpresion}),
+    Hospital.find({name: regularExpresion})
+  ]);
 
   res.json({
     ok: true,
-    message: search
+    users,
+    doctors,
+    hospitals
   });
 }
 
