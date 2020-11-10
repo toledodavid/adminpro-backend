ed61120c246590583b847870fb8fee6d11f2bc8c
David Toledo <dtoledoz0207@gmail.com>
2020-11-09 22:07:46 -0600

    create-public-folder-for-Google-signup-Demo

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(express.json());
 // Database
 dbConnection();
 
+// Public directory
+app.use(express.static('public'));
+
 //Routes
 app.use('/api/users', require('./routes/users.routes'));
 app.use('/api/hospitals', require('./routes/hospitals.routes'));
